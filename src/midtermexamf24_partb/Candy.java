1d62ca0b4ab5c88cab64ca38b888bdfdd75df9a8
Lazaro Yovanys Carabeo Vazquez <carabeol@sheridancollege.ca>
2024-10-15 09:41:16 -0400

    selectItem implemnted

---
+++ src/midtermexamf24_partb/Candy.java
@@ -0,0 +1,12 @@
+package midtermexamf24_partb;
+
+public class Candy {
+    private enum candies{
+        CHOCOLATE_BAR,SOUR_CANDY, SOFT_DRINK, POTATO_CHIPS
+    };
+
+    public Candy(){
+
+    }
+
+}
--- src/midtermexamf24_partb/VendingMachineItem.java
+++ src/midtermexamf24_partb/VendingMachineItem.java
@@ -9,13 +9,23 @@ import java.util.Scanner;
 
 public class VendingMachineItem {
     public double price;
-    
+
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
     
     public VendingMachineItem() {
         // Constructor left blank intentionally
+    }
+
+    public String selectItem(String item) {
+        for (int i = 0; i < candies.length; i++) {
+            if (itemAvailability[i]==true) {
+                return candies[i] + "is available and price is" + prices[i];
+            }
+
+        }
+        return  "The item is not available";
     }
     
     public double getPrice() {
@@ -27,10 +37,16 @@ public class VendingMachineItem {
     }
     
     public static void displayMenu() {
+        Scanner scanner = new Scanner(System.in);
+        VendingMachineItem v = new VendingMachineItem();
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
         for (int i = 0; i < 4; i++) {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
+        System.out.println("Please enter the item you would like to purchase: ");
+        String item = scanner.nextLine();
+        v.selectItem(item);
+
     }
     
     
