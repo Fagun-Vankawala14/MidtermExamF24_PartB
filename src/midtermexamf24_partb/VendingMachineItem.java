6373f2970b997edcc448bb4c2371409d0339e117
dhruvi-mnv <jariwadh@sheridancollege.ca>
2024-10-15 10:27:54 -0400

    Second Commit - Added item availability check and selection feature

--- src/midtermexamf24_partb/VendingMachineItem.java
+++ src/midtermexamf24_partb/VendingMachineItem.java
@@ -26,18 +26,56 @@ public class VendingMachineItem {
         price = givenPrice;
     }
     
+    public boolean selectItem(int itemId) {
+        if (isAvailable(itemId)) {
+            // Proceed with selecting the item
+            System.out.println("You selected: " + candies[itemId]);
+            itemAvailability[itemId] = false;
+            return true;
+        } else {
+            System.out.println("Item is not available.");
+            return false;
+        }
+    }
+
+    private boolean isAvailable(int itemId) {
+        // Logic to check availability of the item (e.g., checking stock or status)
+        return itemAvailability[itemId];
+    }
+
+    
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
+        for (int i = 0; i < candies.length; i++) {
+            String availability = itemAvailability[i] ? "Available" : "Out of stock";
+            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i] + " (" + availability + ")");
         }
     }
     
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        displayMenu();
-                
-
-      }
+        VendingMachineItem vendingMachine = new VendingMachineItem();
+        
+        while (true) {
+            displayMenu();
+            System.out.print("Select an item by number (1 to " + candies.length + ") or press 0 to exit: ");
+            int selection = sc.nextInt() - 1;
+            
+            if (selection == -1) {
+                System.out.println("Exiting...");
+                break;
+            }
+            
+            if (selection >= 0 && selection < candies.length) {
+                vendingMachine.selectItem(selection); // Check item availability and select if possible
+            } else {
+                System.out.println("Invalid selection. Please choose a valid number.");
+            }
+            
+        }
+        
+        sc.close();
+    }
+    
 }
