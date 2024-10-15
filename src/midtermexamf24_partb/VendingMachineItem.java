aa20fbdaab957498654c9707a7b01f8c8dc48c5b
Palakpreet Kaur <kau17085@sheridancollege.ca>
2024-10-15 08:57:27 -0500

    New Branch Created

--- src/midtermexamf24_partb/VendingMachineItem.java
+++ src/midtermexamf24_partb/VendingMachineItem.java
@@ -1,4 +1,5 @@
 /*
+   991744283_Palakpreet_Midterm(Branch Name)
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
