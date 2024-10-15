13b9aed993fe80b7eb709b768fa2cb7f22c5c019
XipraThakkar <thakkarx@sheridancollege.ca>
2024-10-15 10:15:30 -0400

    Second Commit (Step 5). selectItem() method added.

--- src/midtermexamf24_partb/VendingMachineItem.java
+++ src/midtermexamf24_partb/VendingMachineItem.java
@@ -29,15 +29,41 @@ public class VendingMachineItem {
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
         for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
+            String available;
+            if(itemAvailability[i]){
+                available="Available";
+            } else {
+                available="Out of Stock";
+            }
+            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i] + " ("+available+").");
         }
     }
     
+    public static void selectItem(int noOfItem){
+        if(noOfItem<1 || noOfItem>4){
+            System.out.println("Your selection is invalid. Please try again.");
+        }
+        
+        if (itemAvailability[noOfItem - 1]) {
+            System.out.println("You selected " + candies[noOfItem - 1] + ". Enjoy!");
+            itemAvailability[noOfItem - 1] = false;
+        } else {
+            System.out.println("Sorry, " + candies[noOfItem - 1] + " is out of stock.");
+        }
+    }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        displayMenu();
-                
-
+        int number;
+        do{
+            displayMenu();
+            System.out.println("If you want to exit, choose 5.");
+            number = sc.nextInt();
+            if(number==5){
+                System.out.println("See you later. Bye!");
+                break;
+            }
+            selectItem(number);
+        } while(number!=5);
       }
 }
