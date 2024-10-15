c096cce595369ef0263f0964bb0756fcdbc0f862
preet <damadama@sheridancollege.ca>
2024-10-15 10:56:21 -0400

    modified selectItem method.

---
+++ src/midtermexamf24_partb/VendingMachine.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexamf24_partb;
+
+import java.util.Scanner;
+import static midtermexamf24_partb.VendingMachineItem.displayMenu;
+import static midtermexamf24_partb.VendingMachineItem.selectItem;
+
+/**
+ *
+ * @author preet
+ */
+public class VendingMachine {
+    
+    // We have made a separate class for main method for loose coupling and increase cohesion of VendingMachineItem class  because by making the separate class for
+    //main method, now if user wants to use the code written in VendingMachineItem class but without running the main method in the below way he 
+    //can easily do so because now both are in separate classes
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        displayMenu();
+        Item();
+        selectItem();       
+
+      }
+    
+}
--- src/midtermexamf24_partb/VendingMachineItem.java
+++ src/midtermexamf24_partb/VendingMachineItem.java
@@ -9,7 +9,9 @@ import java.util.Scanner;
 
 public class VendingMachineItem {
     public double price;
-    
+    //This class is showing menu and availability of items in the menu by using the variables defined in the method
+    //. without asking for a user input or generating any random values. It is just showing the items which user can purchase
+    //this means all the methods are functionally related to each other. Therefore this class have high cohesion
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
@@ -32,8 +34,9 @@ public class VendingMachineItem {
             System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
         }
     }
-    public static void selectItem(){
+    public static void Item(){
             System.out.println("Please choose the candies after checking its availability:");
+            
             for (int i = 0; i < 4; i++) {
                 if(itemAvailability[i]==true)
                 {
@@ -44,12 +47,39 @@ public class VendingMachineItem {
                 }
         }
     }
-    
-    //Add new feature to selectItem by checking the availability. 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        displayMenu();
-        selectItem();       
-
-      }
+    public static void selectItem(){
+        String c="chocolate Bar";
+        if(itemAvailability[0]==true)
+                {
+                    System.out.println("chocolate Bar is available. You can take it");
+                }
+                else{
+                    System.out.println("Sorry, chocolate Bar is unavailable. You can not take it");
+                } 
+//        Scanner in =new Scanner(System.in);
+//        System.out.println("The customer wants to take a chocolate candy");
+//        String candy= in.nextLine();
+//            if(candy=="chocolate Bar")
+//            {
+//                if(itemAvailability[0]==true)
+//                { 
+//                    return "chocolate Bar is available. You can purchase it.";
+//                }
+//                else{
+//                    return "chocolate Bar is  not available. You can not purchase it.";
+//                }
+//            }
+//            if(candy =="sour candy"){
+//                if(itemAvailability[1]==true)
+//                { 
+//                    return "sour candy is available. You can purchase it.";
+//                }
+//                else{
+//                     return "sour candy is not available. You can not purchase it.";
+//                }
+//            }
+//            return"sd";
+//    }
+//  
+}
 }
